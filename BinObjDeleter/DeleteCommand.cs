9c0157769a8f5193753408d31a9712bf11995b43
Ryan Jantz <rjantz@orionlaw.com>
2018-06-11 14:55:11 -0700

    Initial commit.

---
+++ BinObjDeleter/DeleteCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide;
+
+namespace BinObjDeleter
+{
+    public enum DeleteCommands
+    {
+        DeleteCommand,
+    }
+    public class DeleteCommandHandler : CommandHandler
+    {
+
+        protected override void Run()
+        {
+            base.Run();
+            var projects = IdeApp.Workspace.GetAllProjects();
+            foreach (var project in projects)
+            {
+                var dir = project.ItemDirectory;
+                if (dir != null)
+                {
+                    try
+                    {
+                        var binPath = dir + "/bin";
+                        var objPath = dir + "/obj";
+                        if (Directory.Exists(binPath))
+                        {
+                            Directory.Delete(binPath, true);
+                        }
+                        if (Directory.Exists(objPath))
+                        {
+                            Directory.Delete(objPath, true);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Diagnostics.Debug.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
